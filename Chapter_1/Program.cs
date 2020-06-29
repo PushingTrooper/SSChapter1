ddc4212bed23136bcbe2d332d01252766470ced9
PushingTrooper <junis.milori@gmail.com>
2020-06-29 14:56:03 +0200

    Until 1.1.3

--- Chapter_1/Program.cs
+++ Chapter_1/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace Chapter_1
 {
@@ -22,6 +24,11 @@ namespace Chapter_1
 
         static void Main(string[] args)
         {
+            waitForAnyTask();
+        }
+
+        private static void multipleThreads()
+        {
             new Thread(new ThreadStart(() =>
             {
                 for (int i = 0; i < _field.Value; i++)
@@ -38,6 +45,192 @@ namespace Chapter_1
                 }
             })).Start();
             Console.ReadKey();
+        }
+
+        private static void threadPooling()
+        {
+            ThreadPool.QueueUserWorkItem((s) =>
+            {
+                Console.WriteLine("Working on a thread from threadpool");
+            });
+            Console.ReadKey();
+        }
+
+        private static void tasks()
+        {
+            Task task = Task.Run(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    Console.Write("*");
+                }
+            });
+
+            task.Wait();
+        }
+
+        private static void taskThatReturns()
+        {
+            Task<int> task = Task.Run(() =>
+            {
+                return 42;
+            });
+
+            Console.WriteLine(task.Result);
+        }
+
+        private static void twoTasksInOne()
+        {
+            Task<int> task = Task.Run(() =>
+            {
+                return 42;
+            }).ContinueWith((i) =>
+            {
+                return i.Result * 2;
+            });
+
+            Console.WriteLine(task.Result);
+        }
+
+        private static void taskWithMultipleContinuation()
+        {
+            Task<int> task = Task.Run(() =>
+            {
+                return 42;
+            });
+            task.ContinueWith((i) =>
+            {
+                Console.WriteLine("Canceled");
+            }, TaskContinuationOptions.OnlyOnCanceled);
+
+            task.ContinueWith((i) =>
+            {
+                Console.WriteLine("Faulted");
+            }, TaskContinuationOptions.OnlyOnFaulted);
+
+            var completedTask = task.ContinueWith((i) =>
+            {
+                Console.WriteLine("Completed");
+            }, TaskContinuationOptions.OnlyOnRanToCompletion);
+
+            completedTask.Wait();
+        }
+
+        private static void parentChildTasks()
+        {
+            Task<Int32[]> parent = Task.Run(() =>
+            {
+                var results = new Int32[3];
+
+                new Task(() => results[0] = 0,
+                    TaskCreationOptions.AttachedToParent).Start();
+
+                new Task(() => results[1] = 1,
+                    TaskCreationOptions.AttachedToParent).Start();
+
+                new Task(() => results[2] = 2,
+                    TaskCreationOptions.AttachedToParent).Start();
+
+                return results;
+            });
+
+            var finalTask = parent.ContinueWith(
+                parentTask =>
+                {
+                    foreach (int i in parentTask.Result)
+                        Console.WriteLine(i);
+                });
+
+            finalTask.Wait();
+        }
+
+        private static void parentWithFactory()
+        {
+            Task<Int32[]> parent = Task.Run(() =>
+            {
+                var results = new Int32[3];
+
+                TaskFactory taskFactory = new TaskFactory(TaskCreationOptions.AttachedToParent,
+                    TaskContinuationOptions.ExecuteSynchronously);
+
+                taskFactory.StartNew(() => results[0] = 0);
+                taskFactory.StartNew(() => results[1] = 1);
+                taskFactory.StartNew(() => results[2] = 2);
+
+                return results;
+            });
+
+            var finalTask = parent.ContinueWith(
+                parentTask =>
+                {
+                    foreach (int i in parentTask.Result)
+                        Console.WriteLine(i);
+                });
+
+            finalTask.Wait();
+        }
+
+        private static void waitForMultipleTasks()
+        {
+            Task[] tasks = new Task[3];
+
+            tasks[0] = Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("1");
+                return 1;
+            });
+
+            tasks[1] = Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("2");
+                return 2;
+            });
+
+            tasks[2] = Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("3");
+                return 3;
+            });
+
+            Task.WaitAll(tasks);
+        }
+
+        private static void waitForAnyTask()
+        {
+            Task<int>[] tasks = new Task<int>[3];
+
+            tasks[0] = Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                return 1;
+            });
+
+            tasks[1] = Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                return 2;
+            });
+
+            tasks[2] = Task.Run(() =>
+            {
+                Thread.Sleep(3000);
+                return 3;
+            });
+
+            while(tasks.Length > 0)
+            {
+                int i = Task.WaitAny(tasks);
+
+                Task<int> completedTask = tasks[i];
+
+                Console.WriteLine(completedTask.Result);
+                var temp = tasks.ToList();
+                temp.RemoveAt(i);
+                tasks = temp.ToArray();
+            }
         }
     }
 }
